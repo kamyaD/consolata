d939b74c106fbfc59c41dd9b3e41daf379a785c2
kamyaD <domisemak@gmail.com>
2025-10-29 21:41:46 +0300

    fix-username issue

--- userauth/forms.py
+++ userauth/forms.py
@@ -6,8 +6,16 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['first_name','last_name','email', 'role','school', 'password1', 'password2']
+        fields = [
+            'first_name', 'last_name', 'email',
+            'role', 'school', 'gender', 'marital_status',
+            'password1', 'password2'
+        ]
 
 
 class EmailAuthenticationForm(AuthenticationForm):
-    username = forms.EmailField(label='Email', max_length=254)
+    username = forms.EmailField(label="Email", max_length=254)
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password']

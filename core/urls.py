d2287897175f8a3b462ba711746a1dc3fe8a6516
kamyaD <domisemak@gmail.com>
2025-11-06 12:51:21 +0300

    fix password reset error

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +29,6 @@ urlpatterns = [
     path('student/', include('student.urls')),
     path('consolata/', include('consolata_admin.urls')),
     path('', include('home.urls')),
+   
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

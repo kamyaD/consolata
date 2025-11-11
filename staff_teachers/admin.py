0e19114371192e7bfec35709c43700122d896e4e
kamyaD <domisemak@gmail.com>
2025-11-11 11:36:47 +0300

    add lecturers claim rates

--- staff_teachers/admin.py
+++ staff_teachers/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TimetableEntry
+from .models import TimetableEntry, ClaimForm
 
 # Register your models here.
 @admin.register(TimetableEntry)
@@ -7,3 +7,9 @@ class TimetableEntryAdmin(admin.ModelAdmin):
     list_display = ['day', 'class_level', 'course_code_first','course_title_first', 'lecturer_first','first_room','course_code_second','course_title_second', 'lecturer_second','second_room','after_break_course_code_first','after_break_course_title_first','after_break_lecturer_first','after_break_first_room','after_break_course_code_second','after_break_course_title_second','after_break_lecturer_second','after_break_second_room',
                     'after_lunch_course_code_first','after_lunch_course_title_first','after_lunch_lecturer_first','lecturer']
     list_filter = ['day', 'class_level', 'lecturer',]
+
+
+@admin.register(ClaimForm)
+class ClaimFormAdmin(admin.ModelAdmin):
+    list_display = ['staff_name','staff_email','month','submitted_at','comments','total','state'] 
+    list_filter = ['staff_name','staff_email','month','submitted_at','comments','total','state']   
--- userauth/admin.py
+++ userauth/admin.py
@@ -5,7 +5,7 @@ from .models import CustomUser
 
 
 class CustomUserAdmin(admin.ModelAdmin):
-    fields = ('first_name', 'last_name','email', 'school', 'role', 'is_active')
-    list_display = ('first_name', 'last_name', 'school','role','email', 'is_active')
+    fields = ('first_name', 'last_name','email', 'school', 'role', 'is_active','rates','qualification')
+    list_display = ('first_name', 'last_name', 'school','role','email', 'is_active','rates', 'qualification')
 admin.site.register(CustomUser,CustomUserAdmin)
 
